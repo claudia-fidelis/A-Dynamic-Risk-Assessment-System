92121a656e617716f488fc4c6e64021736d7aa9b
claudia-fidelis <claudiafidelis17@gmail.com>
2023-06-19 20:36:18 -0300

    Update scoring file

--- scoring.py
+++ scoring.py
@@ -10,16 +10,49 @@ import json
 
 
 
-#################Load config.json and get path variables
+################# Load config.json and get path variables
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
+model_path = os.path.join(config['output_model_path']) 
 test_data_path = os.path.join(config['test_data_path']) 
 
 
-#################Function for model scoring
-def score_model():
-    #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
-    #it should write the result to the latestscore.txt file
+#explanatory variables and target
+features = ['lastmonth_activity', 'lastyear_activity', 'number_of_employees']
+target = 'exited'
 
+################# Function for model scoring
+def score_model():
+    '''
+    Take the trained model, load test data and calculate an F1 score for the model relative to the test data
+    Write the result to the latestscore.txt file
+
+    input: None
+    output: Final file latestscore.txt
+    '''
+
+    #load trained model
+    with open(os.path.join(model_path, 'trainedmodel.pkl'), 'rb') as f:
+        model = pickle.load(f)
+
+    #load test data
+    df_test = pd.read_csv(os.path.join(test_data_path, 'testdata.csv'))
+    df_test = df_test.drop(columns=['corporation'])
+
+    X_test = df_test[features]
+    y_test = df_test[target]
+
+    #score test data and calculate F1 score
+    y_pred = model.predict(X_test)
+    f1score = metrics.f1_score(y_pred, y_test)
+
+    #save the f1 score
+    with open(os.path.join(model_path, "latestscore.txt"), "w") as score_file:
+        score_file.write(str(f1score) + "\n")
+    
+    return f1score
+
+if __name__ == '__main__':
+    f1 = score_model()
+    print(f1)
